262ee878ccf71158a642c7c1c6f8b6b29302749a
MaxBakshaev <risen.2011@mail.ru>
2025-09-16 13:24:12 +0900

    Фикс запуска тестов в экшенах

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -8,9 +8,9 @@ client = TestClient(application)
 
 def test_app_docs_available():
     response = client.get("/docs")
-    assert response.status_code, 200
+    assert response.status_code == 200
 
 
 def test_app_redoc_available():
     response = client.get("/redoc")
-    assert response.status_code, 200
+    assert response.status_code == 200
