b7b7c747fee122578fa982d5af8860218071d116
MaxBakshaev <risen.2011@mail.ru>
2025-09-16 11:54:18 +0900

    Подключение основного роутера к main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
 
+from api import router as api_router
+
+
+app = FastAPI()
+app.include_router(
+    api_router,
+    prefix="/api",
+)
 
 application = FastAPI()
+application.include_router(api_router)
 
 
 def run():
